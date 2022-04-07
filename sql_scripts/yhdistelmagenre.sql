edec73f48b4eeffd104792b9fa2efa688679556e
Annala <n1anje00@students.oamk.fi>
2022-04-07 15:18:23 +0300

    Changed table and folders locations

--- sql_scripts/yhdistelmagenre.sql
+++ sql_scripts/yhdistelmagenre.sql
@@ -2,6 +2,6 @@ CREATE TABLE yhdistelmagenre (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     genre_id int NOT NULL,
     peli_id int NOT NULL,
-    FOREIGN KEY (peli) REFERENCES peli(id),
-     FOREIGN KEY (genre) REFERENCES genre(id)
+    FOREIGN KEY (peli_id) REFERENCES peli(id),
+     FOREIGN KEY (genre_id) REFERENCES genre(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_sv_0900_ai_ci
