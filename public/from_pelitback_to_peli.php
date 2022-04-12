1c0c8f70b10c23a564b20543e8b7a8f654dfb213
Olli-Pekka <n1haol00@students.oamk.fi>
2022-04-12 13:54:49 +0300

    Migrated pelitback into peli & yhdistelmagenre

---
+++ public/from_pelitback_to_peli.php
@@ -0,0 +1,57 @@
+<?php
+
+require '../src/modules/db.php';
+
+$pdo = getPdoConnection();
+
+$sql = "SELECT * FROM pelitback";
+$pelitback = $pdo->query($sql);
+
+$pelin_nimi;
+$tyylilaji = array();
+$ikasuositus;
+$konsolitunniste;
+
+if($pelitback->rowCount() > 0) {
+    try {
+        $pdo->beginTransaction();
+        // $sql = "SET @peli_id AS INT";
+        // $statement = $pdo->prepare($sql);
+        // $statement->execute();
+        while ($row = $pelitback->fetch()) {
+            $pelin_nimi = $row["pelin_nimi"];
+            $tyylilaji = explode(" / ", $row["tyylilaji"]);
+            $ikasuositus = $row["ikasuositus"];
+            $konsolitunniste = $row["tunniste"];
+
+            $sql = "INSERT INTO peli (nimi, ikasuositus, konsolitunniste) VALUES (?, ?, ?)";
+            $statement = $pdo->prepare($sql);
+            $statement->bindParam(1, $pelin_nimi);
+            $statement->bindParam(2, $ikasuositus);
+            $statement->bindParam(3, $konsolitunniste);
+            $statement->execute();
+
+            echo $pelin_nimi . " ";
+
+            $peli_id = $pdo->lastInsertId();
+
+            // $sql = "SET @peli_id = LAST_INSERT_ID()";
+            // $statement = $pdo->prepare($sql);
+            // $statement->execute();
+
+            for($i = 0; $i < count($tyylilaji); $i++) {
+                $sql = "INSERT INTO yhdistelmagenre (genre_id, peli_id) VALUES (?, ?)";
+                $statement = $pdo->prepare($sql);
+                $statement->bindParam(1, $tyylilaji[$i]);
+                $statement->bindParam(2, $peli_id);
+                $statement->execute();
+                echo " " . $tyylilaji[$i];
+            }
+            echo "<br>";
+        }
+        $pdo->commit();
+    } catch (\Throwable $e) {
+        $pdo->rollback();
+        throw $e;
+    }
+}
