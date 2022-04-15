6b6db6fce144cce0cf89dd2b3143b00618c7c665
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-04-15 16:26:32 +0300

    clean up sql scripts

--- sql_scripts/istunto_kayttaja.sql
+++ sql_scripts/istunto_kayttaja.sql
@@ -3,6 +3,6 @@ CREATE TABLE istunto_kayttaja (
 id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 tunnus varchar(150) NOT NULL,
 email varchar(35) DEFAULT NULL,
-admin VARCHAR(150) NOT NULL,
+oikat VARCHAR(150) NOT NULL,
 password varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_sv_0900_ai_ci
