cf875b81620288331487a8835fa48b9988630290
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2022-04-24 19:50:43 +0300

    added form tags

--- public/peli.php
+++ public/peli.php
@@ -1,13 +1,12 @@
 
 <?php
-    INCLUDE MODULES_DIR . 'db.php';
+    include MODULES_DIR . 'db.php';
     include TEMPLATES_DIR . 'header.php';
     //include TEMPLATES_DIR . 'konsolit_table.php';
     include TEMPLATES_DIR . 'konsolihaku.php';
     include TEMPLATES_DIR . 'nimihaku.php';
-
+    include TEMPLATES_DIR . 'pelihaku.php';
     
-
     $pdo = getPdoConnection();
 
     //Haetaan valikkoon lista valmistajista
@@ -16,15 +15,14 @@
     $pvalmistaja = $pdo->query($sql);
 
     if ($pvalmistaja->rowCount() > 0) {
-        echo '<label for="valmistaja" class="col-sm-2 col-form-label">Pelit valmistajittain:</label>
-    <select name="pvalmistaja">';
+        echo '<form method="post"><label for="valmistaja" class="col-sm-2 col-form-label">Pelit valmistajittain:</label>
+        <select name="pvalmistaja">';
 
         foreach ($pvalmistaja as $row) {
             echo '<option value="' . $row["valmistaja"] . '">' . $row["valmistaja"] . '</option>';
         }
-        echo '<option value="Valitse" selected="selected">Valitse</option></select><br/>';
+        echo '<option value="Valitse" selected="selected">Valitse</option></select></form>';
     }
-
 
     //Haetaan valikkoon lista konsoleista
     $sql = "SELECT DISTINCT malli FROM konsolitunniste WHERE malli NOT LIKE '%Hero%' AND malli NOT LIKE '%Groud%' AND malli NOT LIKE '%Micro%' AND malli NOT LIKE '%One%' ORDER BY malli asc";
@@ -32,15 +30,14 @@
     $pmalli = $pdo->query($sql);
 
     if ($pmalli->rowCount() > 0) {
-        echo '<label for="malli" class="col-sm-2 col-form-label">Pelit konsoleittain:</label>
-    <select name="pmalli">';
+        echo '<form method="post"><label for="malli" class="col-sm-2 col-form-label">Pelit konsoleittain:</label>
+        <select name="pmalli">';
 
         foreach ($pmalli as $row) {
             echo '<option value="' . $row["malli"] . '">' . $row["malli"] . '</option>';
         }
-        echo '<option value="Valitse" selected="selected">Valitse</option></select><br/>';
+        echo '<option value="Valitse" selected="selected">Valitse</option></select></form>';
     }
-
 
     //Haetaan valikkoon tyylilajit
     $sql = "SELECT DISTINCT tyylilaji FROM genre ORDER BY tyylilaji asc";
@@ -48,15 +45,14 @@
     $genre = $pdo->query($sql);
 
     if ($genre->rowCount() > 0) {
-        echo '<label for="tyylilaji" class="col-sm-2 col-form-label">Pelit tyylilajeittain:</label>
-    <select name="tyylilaji">';
+        echo '<form method="post"><label for="tyylilaji" class="col-sm-2 col-form-label">Pelit tyylilajeittain:</label>
+        <select name="tyylilaji">';
 
         foreach ($genre as $row) {
             echo '<option value="' . $row["tyylilaji"] . '">' . $row["tyylilaji"] . '</option>';
         }
-        echo '<option value="Valitse" selected="selected">Valitse</option></select><br/>';
+        echo '<option value="Valitse" selected="selected">Valitse</option></select></form>';
     }
-
 
     //Haetaan valikkoon lista valmistajista
     $sql = "SELECT DISTINCT valmistaja FROM konsolitunniste WHERE malli NOT LIKE '%PC%' ORDER BY valmistaja asc";
@@ -65,7 +61,7 @@
 
     if ($konsoli->rowCount() > 0) {
         echo '<form method="post"><label for="konsoli" class="col-sm-2 col-form-label">Konsolit:</label>
-    <select name="konsoli">';
+        <select name="konsoli">';
 
         foreach ($konsoli as $row) {
             echo '<option value="' . $row["valmistaja"] . '">' . $row["valmistaja"] . '</option>';
@@ -73,11 +69,9 @@
         echo '<option value="pelikonsoli">Pelikonsolit</option><option value="kasikonsoli">Käsikonsolit</option><option value="konsoli">Kaikki</option><option value="Valitse" selected="selected">Valitse</option></select></form>';
     }
 
-
     echo '<form method="post"><label for="nimihaku" class="col-sm-2 col-form-label">Nimihaku:</label><input type ="text" name="pelin_nimi"/></form>';
 
-    echo '<form method="post"><div class="col-sm-2 "><input type="submit" name="Hae" value="Hae"/></form></div>';
-
+    echo '<form action="peli.php" method="post"><div class="col-sm-2 "><input type="submit" name="Hae" value="Hae"/></form></div>';
 
 
     include TEMPLATES_DIR . 'footer.php';
